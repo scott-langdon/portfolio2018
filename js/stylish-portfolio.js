9e18b02f17801de09a203293176e99c81cabbe77
Scott Langdon <scott.langdon@tylt.com>
2018-04-19 14:37:12 -0700

    edit

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -8,11 +8,12 @@
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
   });
-
+  
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      console.log(target);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
